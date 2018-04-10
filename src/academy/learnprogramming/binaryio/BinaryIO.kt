94a175bafe80d3a08fb59a925efaf421050dc66b
Robert Lowe <robertmlowe@rmlowe.com>
2018-04-10 18:07:17 +0800

    Reading Binary Files and Try with Resources

--- src/academy/learnprogramming/binaryio/BinaryIO.kt
+++ src/academy/learnprogramming/binaryio/BinaryIO.kt
@@ -1,6 +1,25 @@
 package academy.learnprogramming.binaryio
 
+import java.io.DataInputStream
+import java.io.EOFException
+import java.io.FileInputStream
+
 
 fun main(args: Array<String>) {
-    
+
+    val di = DataInputStream(FileInputStream("testfile.bin"))
+    var si: String
+
+    try {
+        while (true) {
+            si = di.readUTF()
+            println(si)
+        }
+    }
+    catch (e: EOFException) {
+
+    }
+
+    try (fr: FileReader = )
+
 }
